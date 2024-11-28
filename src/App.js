7f1114e8f719e07e0603dc268367c3863287123f
Ava Gonick <144552066+avagonick@users.noreply.github.com>
2024-11-27 23:01:52 -0800

    Adding the Website from old repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {WebHeader} from './WebHeader';
+import {Home} from './Home';
+import {GetStarted} from './GetStarted';
+import {Footer} from './Footer';
+import {AboutMe} from './AboutMe';
+import {Gallery} from './Gallery';
+import {Intro} from './Intro';
+
+function App() {
+  return (
+    <>
+    <WebHeader/>
+    <Home/>
+    <GetStarted/>
+    <AboutMe/>
+    <Gallery/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+export function Footer(){
+return (
+<footer id="Footer">
+    <div id = "FooterContainer">
+        <div><a href = "https://form.jotform.com/242305756247155" target= "_blank" id = "CreditForm">Inquiry Form</a></div>
+        <div className = "Sep">|</div>
+        <div id = "PhoneNumber"> hatecoachseats@gmail.com</div>
+        <div className = "Sep">|</div>
+        <div id = "MadeBy">Developed by Ava, Lillian, and Max Gonick</div>
+    </div>
+    <div id = "CopyWrite">&#169;2024 Business Class On Points</div>
+</footer>
+    )
+}
