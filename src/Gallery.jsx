bbc575054d417ba51b454bb6be5d503343ac2cd4
maxgonick <maxwellgonick@g.ucla.edu>
2025-10-30 14:11:39 -0700

    rename jpg

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,14 +1,14 @@
 import "./Gallery.css";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
-import pic1 from "./Images/pic1.JPG";
-import pic2 from "./Images/pic2.JPG";
-import pic3 from "./Images/pic3.JPG";
-import pic4 from "./Images/pic4.JPG";
+import pic1 from "./Images/pic1.jpg";
+import pic2 from "./Images/pic2.jpg";
+import pic3 from "./Images/pic3.jpg";
+import pic4 from "./Images/pic4.jpg";
 import pic5 from "./Images/pic5.jpg";
 import pic6 from "./Images/pic6.jpg";
 import pic7 from "./Images/pic7.jpg";
-import video1 from "./Images/video.MP4";
+import video1 from "./Images/video.mp4";
 
 export function Gallery() {
   const images = [
